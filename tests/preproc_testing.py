1bc6059269852666cd9b5325279c263036b19f05
JCoetzee123 <39802383+JCoetzee123@users.noreply.github.com>
2019-07-25 10:19:31 +0200

    Add files via upload

--- tests/preproc_testing.py
+++ tests/preproc_testing.py
@@ -4,24 +4,30 @@ from spira.core.parameters.processors import ProcessorFloat
 from spira.core.parameters.processors import ProcessorInt
 
 class Layer(spira.ParameterInitializer):
-   integer = spira.IntegerParameter(default= "abcder", preprocess=spira.ProcessorInt())
-   floateger = spira.FloatParameter(default = "2.12", preprocess = spira.ProcessorFloat())
+   integer = spira.IntegerParameter(default= 1, preprocess=spira.ProcessorInt())
+   floateger = spira.FloatParameter(default = 2.12, preprocess = spira.ProcessorFloat())
 
 layer = Layer()
-print(layer.integer)
 
 def test_int_invalidStr():
     with pytest.raises(ValueError) as e:
-        layer.interger = "1.2"
+            layer.integer = "1.2"
 
 def test_int_float():
     layer.interger = 1.2
-    assert layer.interger == 1
+    print(layer.integer)
+    assert layer.integer == 1
 
 def test_int_validStr():
-    layer.interger = "4"
-    assert layer.interger == 4
+    layer.integer = "4"
+    assert layer.integer == 4
 
+def test_float_float():
+    layer.floateger = 1.2
+    assert layer.floateger == 1.2
 
+def test_float_validStr():
+    layer.floateger = "4.2"
+    assert layer.floateger == 4.2
 
 
